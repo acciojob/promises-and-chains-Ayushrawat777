0dca018c589f1945ec13a3717b8be7cb96850487
AYUSH <106034287+Ayushrawat777@users.noreply.github.com>
2024-05-08 00:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+ function myFunction(e){
+      
+      e.preventDefault();
+      let name= document.getElementById('name').value
+      let age= document.getElementById('age').value
+
+  let myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (age>18) {
+          resolve(alert(`Welcome, ${name} .You can vote`));
+        } else { 
+          reject(alert(`Oh sorry ${name} .You aren't old enough`));
+        }
+      }, 4000);
+    });
+
+    myPromise.then((data) => {
+      console.log(data);
+    })
+  }
